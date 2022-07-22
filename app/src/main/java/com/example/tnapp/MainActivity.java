1cb9d53bf6c7c534a319764fe2ed0e68bfa4602d
Gui Tri <guillaume.trionfo@gmail.com>
2022-07-22 17:35:00 +0100

    error handling features

--- app/src/main/java/com/example/tnapp/MainActivity.java
+++ app/src/main/java/com/example/tnapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.tnapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -26,34 +30,59 @@ public class MainActivity extends AppCompatActivity {
         String apiKey = "9544faf78f1ab9fbab68a3fab1cc8203";
         String gnewsURL = "https://gnews.io/api/v4/top-headlines?token=" + apiKey;
 
-        OkHttpClient client = new OkHttpClient();
+        if (isNetworkAvailable()) {
 
-        Request request = new Request.Builder()
-                .url(gnewsURL)
-                .build();
+            OkHttpClient client = new OkHttpClient();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
+            Request request = new Request.Builder()
+                    .url(gnewsURL)
+                    .build();
 
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
 
-            }
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
 
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                try {
-
-                    if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "IO Exception caught: ", e);
                 }
-            }
-        });
 
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    try {
+                        Log.v(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                    }
+                }
+            });
+        }
         Log.d(TAG, "Main UI code is running!");
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getSupportFragmentManager(), "error_dialog");
     }
 }
